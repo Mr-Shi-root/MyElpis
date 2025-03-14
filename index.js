3a05306304d700889e68e82c716e80ca1128d09a
MrShi <mrshi@MrShideMBP.bbrouter>
2025-03-14 17:36:01 +0800

    feat: index.js入口文件

---
+++ index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+
+// koa 实例
+const app = new Koa();
+
+// 启动服务
+try {
+    const port = process.env.PORT || 8080;
+    const host = process.env.IP || '0.0.0.0';
+    console.log(`Server running on http://${host}:${port}`);
+    console.log(`Search ${process.env.HOST} & ${process.env.IP}`);
+    app.listen(port, host)
+} catch (error) {
+    console.log(error);
+    
+}
