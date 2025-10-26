4eb3fce5e354daeeb137f6d17d84074ee4b55600
宁 <sjn18242093868@163.com>
2025-10-27 03:25:37 +0800

    fix: 修复fix

--- elpis-core/loaders/extend.js
+++ elpis-core/loaders/extend.js
@@ -30,7 +30,6 @@ module.exports = (app) => {
     const fileList = glob.sync(path.resolve(extendPath, `.${sep}**.js`));
 
     // 遍历所有文件目录，把内容加载到 app.extend 下
-    const extend = {};
     fileList.forEach(file => {
 
         // 提取文件名称
