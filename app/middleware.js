41eb5a4a7220335d180e2b56eb3ff543c49910ff
宁 <sjn18242093868@163.com>
2025-10-28 08:21:17 +0800

    feat: 配置静态资源初始路径

--- app/middleware.js
+++ app/middleware.js
@@ -2,6 +2,10 @@ const path = require('path');
 const { sep } = path;
 
 module.exports = (app) => { 
+    // 配置静态根目录
+    const koaStatic = require('koa-static');
+    app.use(koaStatic(path.resolve(process.cwd(), `.${sep}app${sep}public`)));
+
     //  注册 中间件
     const koaNunjucks = require('koa-nunjucks-2');
     // 模版渲染引擎
