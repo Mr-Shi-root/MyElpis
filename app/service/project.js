59ea69189facf32e91f7980dda2ffab7e3f1ffe2
宁 <sjn18242093868@163.com>
2025-11-07 01:12:57 +0800

    feat: 增加 model 模型继承

--- app/service/project.js
+++ app/service/project.js
@@ -1,5 +1,9 @@
 module.exports = (app) => {
     const BaseService = require('./base')(app);
+
+    const ModelList = require('../../model/index.js')(app);
+    console.log(JSON.stringify(ModelList));
+    
     return class ProjectService extends BaseService {
         async getList() {
             return [
